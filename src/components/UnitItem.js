a9aa0f46df1f9c87b83b6ffb7626e952d68a151d
JoseMa <esparev@hotmail.com>
2022-11-12 14:51:19 -0600

    refactor: ♻️ agregar funcionalidad al componente de unidades

--- src/components/UnitItem.js
+++ src/components/UnitItem.js
@@ -1,5 +1,42 @@
 import React from 'react';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+import globalStyles from '../styles/globalStyles';
+import itemStyles from '../styles/itemStyles';
 
-const UnitItem = () => {};
+const UnitItem = props => {
+  const {number, onRoute, location} = props;
+
+  return (
+    <TouchableOpacity style={itemStyles.entityItem}>
+      <View style={itemStyles.entityInfo}>
+        {onRoute ? (
+          <Image
+            style={itemStyles.entityIcon}
+            source={require('../assets/icons/side-bus-active-black.png')}
+          />
+        ) : (
+          <Image
+            style={itemStyles.entityIcon}
+            source={require('../assets/icons/side-bus-inactive.png')}
+          />
+        )}
+        <View style={itemStyles.entityLocationDistance}>
+          <Text style={globalStyles.textRegular}>Unidad {number}</Text>
+          {onRoute ? (
+            <Text style={globalStyles.textSmall}>
+              En ruta • Cerca de {location}
+            </Text>
+          ) : (
+            <Text style={globalStyles.textSmall}>Fuera de servicio</Text>
+          )}
+        </View>
+      </View>
+      <Image
+        style={itemStyles.seeEntity}
+        source={require('../assets/icons/chevron.png')}
+      />
+    </TouchableOpacity>
+  );
+};
 
 export default UnitItem;
