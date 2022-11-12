0ce07d79d6222e0bbcfbe86a1f43b5b08f120694
JoseMa <esparev@hotmail.com>
2022-11-12 13:07:23 -0600

    feat: ✨ crear pantalla de perfil

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,58 @@
 import React from 'react';
+import {Image, Pressable, Text, View} from 'react-native';
+import globalStyles from '../styles/globalStyles';
+import profileStyles from '../styles/profileStyles';
 
-const Profile = () => {};
+const Profile = () => {
+  return (
+    <View style={[globalStyles.body, profileStyles.screen]}>
+      {/* Profile */}
+      <View style={profileStyles.profile}>
+        <Pressable style={profileStyles.image}>
+          <Image
+            style={profileStyles.profileImg}
+            source={require('../assets/icons/oscar.png')}
+          />
+          <Image
+            style={profileStyles.editIcon}
+            source={require('../assets/icons/edit.png')}
+          />
+        </Pressable>
+        <View>
+          <Text style={globalStyles.h1}>Oscar Isaac Hernández Estrada</Text>
+        </View>
+      </View>
+      {/* Info */}
+      <View style={profileStyles.infoContainer}>
+        <View style={profileStyles.info}>
+          <Text style={globalStyles.textSmall}>Nombre</Text>
+          <Text style={globalStyles.textRegular}>
+            Oscar Isaac Hernández Estrada
+          </Text>
+        </View>
+        <View style={profileStyles.info}>
+          <Text style={globalStyles.textSmall}>Nombre de usuario</Text>
+          <Text style={globalStyles.textRegular}>oscar.isaac</Text>
+        </View>
+        <View style={profileStyles.info}>
+          <Text style={globalStyles.textSmall}>Correo electrónico</Text>
+          <Text style={globalStyles.textRegular}>oscar.isaac@gmail.com</Text>
+        </View>
+        <View style={profileStyles.info}>
+          <Text style={globalStyles.textSmall}>Contraseña</Text>
+          <Text style={globalStyles.textRegular}>••••</Text>
+        </View>
+      </View>
+
+      <Pressable style={profileStyles.logoutBtn}>
+        <Text style={profileStyles.logoutTxt}>Cerrar Sesión</Text>
+        <Image
+          style={profileStyles.logoutIcon}
+          source={require('../assets/icons/logout.png')}
+        />
+      </Pressable>
+    </View>
+  );
+};
 
 export default Profile;
---
+++ src/styles/profileStyles.js
@@ -0,0 +1,62 @@
+import {StyleSheet} from 'react-native';
+
+const profileStyles = StyleSheet.create({
+  screen: {
+    position: 'relative',
+    padding: 20,
+  },
+  profile: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 12,
+    paddingBottom: 12,
+    borderBottomColor: '#737373',
+    borderBottomWidth: 0.5,
+  },
+  image: {
+    marginRight: 12,
+    marginBottom: 12,
+  },
+  profileImg: {
+    position: 'relative',
+    width: 64,
+    height: 64,
+    margin: 2,
+  },
+  editIcon: {
+    position: 'absolute',
+    bottom: 0,
+    width: 24,
+    height: 24,
+  },
+  infoContainer: {
+    paddingTop: 20,
+  },
+  info: {
+    marginBottom: 20,
+  },
+  logoutBtn: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 20,
+    borderRadius: 8,
+    backgroundColor: '#222227',
+  },
+  logoutTxt: {
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 16,
+    padding: 8,
+    color: '#FFFFFF',
+  },
+  logoutIcon: {
+    width: 24,
+    height: 24,
+  },
+});
+
+export default profileStyles;
