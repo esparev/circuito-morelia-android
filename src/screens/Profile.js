ccba409988e0c99891070370b65da23ac98f5213
JoseMa <esparev@hotmail.com>
2022-12-03 15:16:03 -0600

    feat: ✨ obtener datos de perfil

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,42 +1,57 @@
 import React from 'react';
 import {Image, Pressable, Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import useAuth from '../hooks/useAuth';
 import globalStyles from '../styles/globalStyles';
 import profileStyles from '../styles/profileStyles';
 
 const Profile = () => {
+  const {auth, logout} = useAuth();
+  const navigation = useNavigation();
+
+  const logoutUser = () => {
+    logout();
+    navigation.navigate('Iniciar Sesión');
+  };
+
   return (
     <View style={[globalStyles.body, profileStyles.screen]}>
       {/* Profile */}
       <View style={profileStyles.profile}>
         <Pressable style={profileStyles.image}>
-          <Image
-            style={profileStyles.profileImg}
-            source={require('../assets/icons/oscar.png')}
-          />
+          {auth.image ? (
+            <Image
+              style={profileStyles.profileImg}
+              source={require('../assets/icons/oscar.png')}
+            />
+          ) : (
+            <Image
+              style={profileStyles.profileImg}
+              source={require('../assets/icons/default-profile.png')}
+            />
+          )}
           <Image
             style={profileStyles.editIcon}
             source={require('../assets/icons/edit.png')}
           />
         </Pressable>
         <View style={profileStyles.name}>
-          <Text style={globalStyles.h1}>Oscar Isaac Hernández Estrada</Text>
+          <Text style={globalStyles.h1}>{auth.name}</Text>
         </View>
       </View>
       {/* Info */}
       <View style={profileStyles.infoContainer}>
         <View style={profileStyles.info}>
           <Text style={globalStyles.textSmall}>Nombre</Text>
-          <Text style={globalStyles.textRegular}>
-            Oscar Isaac Hernández Estrada
-          </Text>
+          <Text style={globalStyles.textRegular}>{auth.name}</Text>
         </View>
         <View style={profileStyles.info}>
           <Text style={globalStyles.textSmall}>Nombre de usuario</Text>
-          <Text style={globalStyles.textRegular}>oscar.isaac</Text>
+          <Text style={globalStyles.textRegular}>{auth.slug}</Text>
         </View>
         <View style={profileStyles.info}>
           <Text style={globalStyles.textSmall}>Correo electrónico</Text>
-          <Text style={globalStyles.textRegular}>oscar.isaac@gmail.com</Text>
+          <Text style={globalStyles.textRegular}>{auth.email}</Text>
         </View>
         <View style={profileStyles.info}>
           <Text style={globalStyles.textSmall}>Contraseña</Text>
@@ -44,8 +59,8 @@ const Profile = () => {
         </View>
       </View>
 
-      <Pressable style={profileStyles.logoutBtn}>
-        <Text style={profileStyles.logoutTxt}>Cerrar Sesión</Text>
+      <Pressable style={profileStyles.logoutBtn} onPress={logoutUser}>
+        <Text style={profileStyles.logoutTxt}>Cerrar sesión</Text>
         <Image
           style={profileStyles.logoutIcon}
           source={require('../assets/icons/logout.png')}
