131a37cb9d50c4d6bc0465279be145d26ecb35a3
JoseMa <esparev@hotmail.com>
2022-12-03 20:54:27 -0600

    feat: ✨ implementar agregar conductor

---
+++ src/components/AddDriverModal.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import {Image, Modal, Pressable, Text, TextInput, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import useAuth from '../hooks/useAuth';
+import axios from 'axios';
+import slugify from 'slugify';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import buttonStyles from '../styles/buttonStyles';
+import modalStyles from '../styles/modalStyles';
+import {envConfig} from '../utils/config';
+
+const AddDriverModal = props => {
+  const {modalVisible, setModalVisible, setAlert} = props;
+  const navigation = useNavigation();
+  const {auth} = useAuth();
+
+  const addDriver = async (url, data, config) => {
+    await axios
+      .post(url, data, config)
+      .then(res => {
+        setModalVisible(false);
+        setAlert('success');
+
+        setTimeout(() => {
+          setAlert('');
+          setTimeout(() => {
+            navigation.navigate('Conductores');
+          }, 500);
+        }, 3000);
+      })
+      .catch(error => {
+        setModalVisible(false);
+        setAlert('error');
+
+        setTimeout(() => {
+          setAlert('');
+        }, 3000);
+      });
+  };
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: data => {
+      const regex = /\s+/g;
+      data.slug = slugify(data.name.replace(regex, '-'), {lower: true});
+      addDriver(`${envConfig.apiUrl}/users`, data, {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      });
+    },
+  });
+
+  return (
+    <View style={modalStyles.modalView}>
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        transparent={true}
+        statusBarTranslucent={true}>
+        <View style={modalStyles.editModal}>
+          <View style={modalStyles.modalContainer}>
+            <View style={modalStyles.modalHeader}>
+              <Text style={modalStyles.modalTitle}>Agregar Unidad</Text>
+              <Pressable onPress={() => setModalVisible(false)}>
+                <Image
+                  style={modalStyles.modalClose}
+                  source={require('../assets/icons/close.png')}
+                />
+              </Pressable>
+            </View>
+            <View style={modalStyles.modalForm}>
+              <View style={modalStyles.modalFormField}>
+                <Text style={modalStyles.modalFormFieldLbl}>Nombre</Text>
+                <TextInput
+                  style={modalStyles.modalFormFieldInput}
+                  placeholder="Ingrese el nombre del conductor"
+                  placeholderTextColor={'#737373'}
+                  value={formik.values.name}
+                  onChangeText={input => formik.setFieldValue('name', input)}
+                />
+                <Text style={modalStyles.modalFormFieldErr}>
+                  {formik.errors.name}
+                </Text>
+              </View>
+              <View style={modalStyles.modalFormField}>
+                <Text style={modalStyles.modalFormFieldLbl}>
+                  Correo electrónico
+                </Text>
+                <TextInput
+                  style={modalStyles.modalFormFieldInput}
+                  placeholder="Ingrese el correo del conductor"
+                  autoCapitalize="none"
+                  placeholderTextColor={'#737373'}
+                  value={formik.values.email}
+                  onChangeText={input => formik.setFieldValue('email', input)}
+                />
+                <Text style={modalStyles.modalFormFieldErr}>
+                  {formik.errors.email}
+                </Text>
+              </View>
+              <View style={modalStyles.modalFormField}>
+                <Text style={modalStyles.modalFormFieldLbl}>Contraseña</Text>
+                <TextInput
+                  style={modalStyles.modalFormFieldInput}
+                  placeholder="Ingrese la contraseña del conductor"
+                  placeholderTextColor={'#737373'}
+                  autoCapitalize="none"
+                  secureTextEntry={true}
+                  value={formik.values.password}
+                  onChangeText={input =>
+                    formik.setFieldValue('password', input)
+                  }
+                />
+                <Text style={modalStyles.modalFormFieldErr}>
+                  {formik.errors.password}
+                </Text>
+              </View>
+              <Pressable
+                style={buttonStyles.btnBgBlack}
+                onPress={formik.handleSubmit}>
+                <Text style={buttonStyles.btnTxt}>Agregar Unidad</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const initialValues = () => {
+  return {slug: '', name: '', email: '', password: '', role: 'driver'};
+};
+
+const validationSchema = () => {
+  return {
+    name: Yup.string().required('Por favor ingrese un nombre'),
+    email: Yup.string()
+      .email('Ingrese un correo válido')
+      .required('Por favor ingrese un correo'),
+    password: Yup.string().required('Por favor ingrese un contraseña'),
+  };
+};
+
+export default AddDriverModal;
--- src/screens/Drivers.js
+++ src/screens/Drivers.js
@@ -1,15 +1,44 @@
-import React, {useState} from 'react';
-import {Pressable, Text, View, ScrollView, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Pressable, Text, Image, View, SafeAreaView} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import DriverList from '../components/DriverList';
+import useAuth from '../hooks/useAuth';
 import useGetUsers from '../hooks/useGetUsers';
+import AddDriverModal from '../components/AddDriverModal';
 import globalStyles from '../styles/globalStyles';
 import tabBarStyles from '../styles/tabBarStyles';
+import alertStyles from '../styles/alertStyles';
+import buttonStyles from '../styles/buttonStyles';
 import {envConfig} from '../utils/config';
 
 const Drivers = () => {
+  const {auth} = useAuth();
   const [activeTab, setActiveTab] = useState(true);
+  const [alert, setAlert] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
   const users = useGetUsers(envConfig.apiUrl);
   const drivers = users.filter(driver => driver.role === 'driver');
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <>
+          {auth.user.role === 'hero' || auth.user.role === 'admin' ? (
+            <Pressable
+              style={buttonStyles.btnBgBlack}
+              onPress={() => setModalVisible(true)}>
+              <Image
+                style={buttonStyles.btnIcon}
+                source={require('../assets/icons/add.png')}
+              />
+              <Text style={buttonStyles.btnTxt}>Agregar</Text>
+            </Pressable>
+          ) : null}
+        </>
+      ),
+    });
+  }, []);
 
   const handleTabToggle = () => {
     setActiveTab(!activeTab);
@@ -55,9 +84,32 @@ const Drivers = () => {
         </Pressable>
       </View>
 
+      <View style={alertStyles.alert}>
+        {alert ? (
+          <View
+            style={
+              alert === 'success'
+                ? [alertStyles.alertContainer, alertStyles.alertGreen]
+                : [alertStyles.alertContainer, alertStyles.alertRed]
+            }>
+            <Text style={alertStyles.alertMsg}>
+              {alert === 'success'
+                ? '¡Conductor agregado exitosamente!'
+                : '¡Ups!, Hubo un error al agregar al conductor.'}
+            </Text>
+          </View>
+        ) : null}
+      </View>
+
       <View style={globalStyles.hero}>
         <DriverList drivers={drivers} />
       </View>
+
+      <AddDriverModal
+        modalVisible={modalVisible}
+        setModalVisible={() => setModalVisible(false)}
+        setAlert={setAlert}
+      />
     </SafeAreaView>
   );
 };
